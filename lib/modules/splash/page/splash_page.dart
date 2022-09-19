6af8e23917fa8ac2a83c7f27a5ac80c73dae6996
wahyuindra <wahyuindra712@gmail.com>
2022-09-19 23:33:43 +0700

    fix splash page

---
+++ lib/modules/splash/page/splash_page.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:bank_sha_main/modules/onboarding/page/onboarding_page..dart';
+import 'package:bank_sha_main/utils/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnBoardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img_logo_dark.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
