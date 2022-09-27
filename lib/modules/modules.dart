2cebdbaf429e881d49c023fbbbbea53a9bb38841
wahyuindra <wahyuindra712@gmail.com>
2022-09-27 23:42:56 +0700

    fix route

--- lib/modules/modules.dart
+++ lib/modules/modules.dart
@@ -1,7 +1,7 @@
 export 'signin/page/signin_page.dart';
 export 'onboarding/page/onboarding_page.dart';
 export 'splash/page/splash_page.dart';
-export 'signin/page/page/sign_up_page.dart';
+export 'signup/page/sign_up_page.dart';
 export 'signup_setprofile/page/sign_up_set_profile_page.dart';
 export 'signup_set_idcard/page/sign_up_set_idcard_page.dart';
 export 'signup_success/page/signup_success_page.dart';
